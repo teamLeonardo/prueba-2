5862d177c52b1ddb0d9361d4620e975e5a71f6ee
leonardo sifuentes <leonardosm3.14@gmail.com>
2022-12-28 11:21:28 -0500

    init todo

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 'use strict'
+const contList = document.querySelector("#cont-list")
+const UlLIST = document.createElement("ul")
+
 const GI = {
-	createElement (tag, atributes) {
-		let element = document
-		return document.createElement(tag)
-	}
+    createElement(tag, atributes) {
+        return document.createElement(tag)
+    }
 }
 
 
@@ -14,4 +16,24 @@ const to_do = [
     'Pasear a perro',
     'Comprar entradas para el cine',
 ]
+
+
+function changeItem(event) {
+    const contLI = event.parentElement
+    if (event.checked === false) {
+        contLI.className = ""
+    }
+    contLI.classList.add("decorar")
+}
+
+to_do.map((value) => {
+    const elementItemString = document.createElement("li")
+    elementItemString.innerHTML = `
+       ${value}   <input type="checkbox" onClick="changeItem(this)" />
+    `
+    UlLIST.appendChild(elementItemString)
+})
+
+contList.appendChild(UlLIST)
+
 
